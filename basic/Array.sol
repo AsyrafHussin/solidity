0ecd81abcee3d85f171d05014d7f69f80f087f48
Asyraf Hussin <asyrafhussin4@gmail.com>
2022-07-30 10:25:13 +0800

    Create Array.sol

---
+++ basic/Array.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Array {
+    // Arrays
+    uint[] public uintArray = [1,2,3];
+    string[] public stringArray = ["apple","banana","carrot"];
+    string[] public values;
+
+    // 2d array
+    uint256[][] public array2D = [[1, 2, 3],[4, 5, 6]]; 
+
+    function addValue(string memory _value) public {
+        values.push(_value);
+    }
+
+    function valueCount() public view returns(uint){
+        return values.length;
+    }
+
+}
