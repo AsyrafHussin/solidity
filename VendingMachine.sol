385a08d6f783b6e3102f1f655a7a0639cef65c0d
Asyraf Hussin <asyrafhussin4@gmail.com>
2022-08-01 10:10:57 +0800

    Create VendingMachine.sol

---
+++ VendingMachine.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.15;
+
+contract VendingMachine {
+    address public owner;
+    mapping(address => uint) donutBalances;
+    uint public immutable donutPrice;
+
+
+    // address(this) = address of current contract
+    constructor(){
+        owner = msg.sender;
+        donutBalances[address(this)] = 100;
+        donutPrice = 1 ether;
+    }
+    
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Only the owner can restock this machine.");
+        _;
+    }
+
+    modifier costs(uint _amount){
+        require(msg.value >= _amount * donutPrice, "Not enough ether provided.");
+        _;
+    }
+
+    modifier checkStock(uint _amount){
+        require(donutBalances[address(this)] >= _amount, "Not enough donut stock to purchase.");
+        _;
+    }
+
+    function getDonutBalance() public view returns (uint){
+        return donutBalances[address(this)];
+    }
+
+    function purchase(uint _amount) public payable costs(_amount) checkStock(_amount){
+        donutBalances[address(this)] -= _amount;
+    }
+
+    function restock(uint _amount) public onlyOwner {
+        donutBalances[address(this)] += _amount; 
+    }
+}
